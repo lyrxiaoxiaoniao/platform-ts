f08db4f6cc87c04001ab8ebff8c84fcbea2c1d08
liuyouren <lyr632537786@163.com>
2019-04-11 21:02:24 +0800

    上传，批量上传成功

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,74 +1,29 @@
 import React, { Component } from 'react'
-
+import { Upload, message, Button, Icon } from 'antd';
 class Home extends Component {
   public render() {
+    const props = {
+      name: 'file',
+      action: 'http://localhost:3000/api/upload',
+      multiple: true,
+      onChange(info: any) {
+        if (info.file.status !== 'uploading') {
+          console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+          message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === 'error') {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+      },
+    };
     return (
       <div>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
-        <p><i>home/page</i></p>
+        <Upload {...props}>
+          <Button>
+            <Icon type="upload" /> Click to Upload
+          </Button>
+        </Upload>
       </div>
     )
   }
