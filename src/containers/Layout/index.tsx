ec07116eab66b3a7786ebb78c4d5af74a6732ffc
liuyouren <lyr632537786@163.com>
2019-04-15 18:04:29 +0800

    update

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -1,7 +1,7 @@
 import './index.scss'
 import React, { Component } from 'react'
 import { Route, withRouter } from 'react-router-dom'
-import { Layout, Icon } from 'antd'
+import { Layout, Icon, Avatar } from 'antd'
 import { routerList } from '@router/index'
 import Menus from '@components/layout/Menus'
 import menus from '@router/menus'
@@ -40,11 +40,14 @@ class LayoutIndex extends Component<any, any> {
               type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
               onClick={this.toggle}
             />
+            <Avatar style={{ backgroundColor: "#7265e6", verticalAlign: 'middle'}} className='avatar'>
+              xiao_liu
+            </Avatar>
           </Header>
           <Content
             style={{ margin: '85px 16px 24px 16px', overflow: 'initial', paddingLeft: (this.state.collapsed) ? '80px' : '200px' }}
           >
-            <div style={{ padding: 24, background: '#fff'}}>
+            <div style={{ padding: 24, background: '#fff' }}>
               {routerList.map(({ path, key, exact, component, ...props }) => (
                 <Route
                   key={path}
