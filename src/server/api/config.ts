ef0f1ad3adfe7e9753cf8664fab808a8e84f2e33
liuyouren <lyr632537786@163.com>
2019-06-19 15:53:04 +0800

    update

--- src/server/api/config.ts
+++ src/server/api/config.ts
@@ -1,4 +1,11 @@
-export default {
-    baseUrl: 'http://192.168.40.130:3000/api',
-    uploadAPi: 'http://192.168.40.130:3000/api/upload'
-}
+const isDev = process.env.NODE_ENV === 'development'
+const uri = isDev
+  ? {
+      baseUrl: 'http://192.168.40.156:3000/api',
+      uploadAPi: 'http://192.168.40.156:3000/api/upload'
+    }
+  : {
+      baseUrl: 'http://www.liuyouren.top/api',
+      uploadAPi: 'http://www.liuyouren.top/api/upload'
+    }
+export default uri
